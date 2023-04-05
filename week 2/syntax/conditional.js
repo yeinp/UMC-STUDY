5d0c42b6c09ee2f6843de345315f36df5eeafa46
yenqkr <102224283+yenqkr@users.noreply.github.com>
2023-04-05 14:44:02 +0900

    Add files via upload

---
+++ week 2/syntax/conditional.js
@@ -0,0 +1,10 @@
+console.log('A');
+console.log('B');
+if(false){
+    console.log('C1');
+}
+else{
+    console.log('C2'); //false라 무시가 됨
+}
+
+console.log('D');
---
+++ week 2/syntax/variable.js
@@ -0,0 +1,5 @@
+var a=1; //var 변수
+console.log(a);
+a=2;
+console.log(a);
+//1=2; 주석 commnet
